e9ece2c4fcd7954881613c64a27b03332804b91a
Aman Gupta <2015aman.a@ves.ac.in>
2022-11-22 20:42:47 +0530

    setting up the repository and service files for the messaging module

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,21 +1,29 @@
 import { Controller, Get, Post, Body, Param  } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messageService: MessagesService;
+
+    constructor(){
+        this.messageService = new MessagesService()
+    }
 
     @Get()
     listAllMessages(){
-
+        return this,this.messageService.findAll()
     }
     
     @Post()
     createMessage(@Body() body: CreateMessageDto){
         console.log(body);
+        return this,this.messageService.create(body.content)
     }
     
     @Get('/:id')
     getMessage(@Param('id') id: string) {
         console.log(id);
+        return this,this.messageService.findOne(id)
     }
 }
