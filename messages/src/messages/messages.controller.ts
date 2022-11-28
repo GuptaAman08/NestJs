4ae56bc4e7545d529803602eb915a14db9fca0a4
Aman Gupta <2015aman.a@ves.ac.in>
2022-11-28 14:00:37 +0530

    clearing the unwanted code and throwing error when unknown message id is passed

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param  } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException  } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -12,18 +12,23 @@ export class MessagesController {
 
     @Get()
     listAllMessages(){
-        return this,this.messageService.findAll()
+        return this.messageService.findAll()
     }
     
     @Post()
     createMessage(@Body() body: CreateMessageDto){
         console.log(body);
-        return this,this.messageService.create(body.content)
+        return this.messageService.create(body.content)
     }
     
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        console.log(id);
-        return this,this.messageService.findOne(id)
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messageService.findOne(id)
+        
+        if (!message){
+            throw new NotFoundException('message not found !!')
+        }
+
+        return message
     }
 }
