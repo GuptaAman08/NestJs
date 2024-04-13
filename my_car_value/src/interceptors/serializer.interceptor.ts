99c122597ffd19c22c5892407202b85244dfda5b
Aman Gupta <2015aman.a@ves.ac.in>
2024-04-13 20:14:16 +0530

    serialization module code changes

---
+++ my_car_value/src/interceptors/serializer.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    // Run something before a request is handled by request handler
+    // console.log('code that runs before any request');
+    return next.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- my_car_value/src/users/users.controller.ts
+++ my_car_value/src/users/users.controller.ts
@@ -8,14 +8,15 @@ import {
   ParseIntPipe,
   Query,
   Delete,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serializer.interceptor';
+import { UserDto } from './dto/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userService: UsersService) {}
 
@@ -24,7 +25,6 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id);
